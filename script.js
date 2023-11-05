509c82d1f1bebd334d5159a582ceca700a3e5905
Anujraghuwanshi2408 <128280445+Anujraghuwanshi2408@users.noreply.github.com>
2023-11-05 13:22:58 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,11 +1,13 @@
 function calculateMinCost() {
   //your code here
+let ropes = document.getElementById("rope-lengths").split(",").map(str => parseInt(str));
+let minCost = 0;
+document.getElementById("result").innerText = ropes
 	
- console.log(ropes);
+ // console.log(ropes);
   
   
 }  
 
-let ropes = document.getElementById("rope-lengths").split(",").map(str => parseInt(str));
-let minCost = 0;
+
 
